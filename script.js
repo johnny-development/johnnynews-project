348b9d9be93e6005deb1fe0ddd336f40b3ec9eb5
Johnny <johnny.development416@gmail.com>
2023-04-27 13:43:20 -0400

    updated to latest deployed files
    pushed older version init and now updated to latest ver of files

---
+++ script.js
@@ -0,0 +1,48 @@
+// const hamburger = document.querySelector(".hamburger");
+// const navMenu = document.querySelector(".nav-menu");
+
+// hamburger.addEventListener("click", () => {
+//   hamburger.classList.toggle("active");
+//   navMenu.classList.toggle("active");
+// });
+
+// document.querySelectorAll(".nav-link").forEach((n) =>
+//   n.addEventListener("click", () => {
+//     hamburger.classList.remove("active");
+//     navMenu.classList.remove("active");
+//   })
+// );
+
+let hamburger = document.querySelector(".hamburger");
+let navMenu = document.querySelector(".nav-menu");
+
+hamburger.onclick = function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+};
+
+document.onclick = function (e) {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+};
+
+hamburger.onscroll = function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+};
+
+document.onscroll = function (e) {
+  if (!hamburger.contains(e.target) && !navMenu.contains(e.target)) {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+  }
+};
+
+// preloader script
+
+var loader = document.getElementById("preloader");
+window.addEventListener("load", function () {
+  loader.style.display = "none";
+});
